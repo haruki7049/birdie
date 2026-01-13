d0ddf6fa97dfa973295d95ddb33ab89d97d1f277
haruki7049 <tontonkirikiri@gmail.com>
2026-01-14 00:41:12 +0900

    Update

--- app/src/main/kotlin/dev/haruki7049/birdie/App.kt
+++ app/src/main/kotlin/dev/haruki7049/birdie/App.kt
@@ -1,6 +1,6 @@
 package dev.haruki7049.birdie
 
 fun main() {
-    val code: MnemonicCode = MnemonicCode.NOP
-    println(code == MnemonicCode.NOP)
+    val code: MnemonicCode = MnemonicCode.ACONST_NULL
+    println(MnemonicCode.fromAddress(1) == MnemonicCode.ACONST_NULL)
 }
--- app/src/main/kotlin/dev/haruki7049/birdie/MnemonicCode.kt
+++ app/src/main/kotlin/dev/haruki7049/birdie/MnemonicCode.kt
@@ -1,5 +1,11 @@
 package dev.haruki7049.birdie;
 
-enum class MnemonicCode {
-    NOP,
+enum class MnemonicCode(val id: Int) {
+    NOP(0),
+    ACONST_NULL(1),
+    ICONST_M1(2);
+
+    companion object {
+        fun fromAddress(id: Int): MnemonicCode? = entries.firstOrNull { it.id == id }
+    }
 }
